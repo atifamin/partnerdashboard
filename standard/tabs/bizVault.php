9f765854c465618993a3524bb8cd2e5363ad44ce
addi <addi.ahmad9@gmail.com>
2019-07-08 14:41:49 +0500

    commit

--- standard/tabs/bizVault.php
+++ standard/tabs/bizVault.php
@@ -1,20 +1,26 @@
 <?php
 include("../includes/header.php");
 include("../includes/top_nav.php");
-include("../includes/side_bar.php");
+//include("../includes/side_bar.php");
 include("../functions/functions.php");
 ?>
 <link rel="stylesheet" href="../assets/css/bizVault.css">
 <div class="row main-area">
-  <div class="col-md-12 logo-area"> <img src="../assets/img/dummy-logo.jpg" alt="" width="150"> </div>
-  <div class="col-md-1">Sidebar</div>
-  <div class="col-md-11">MainSection
-    <div class="col-md-12">top upload bar</div>
-    <div class="col-md-12">Search bar</div>
-    <div class="col-md-9">Main Area</div>
-    <div class="col-md-3">Preview Area</div>
-    <div class="col-md-12">Footer Area</div>
-  </div>
+<header class="logo-area"><img src="../assets/img/dummy-logo.jpg" alt="" width="150"></header>
+    <section class="bizVaultSection">
+      <?php include("bizVault/side-bar.php") ?>
+      <article class="bizVaultArticle">
+        <div class="row">
+            <div class="col-md-12">button bar</div>
+            <div class="col-md-12">search bar</div>
+            <div class="col-md-9">Main Area</div>
+            <div class="col-md-3">Preview Area</div>
+        </div>
+      </article>
+    </section>
+    <footer class="bizVaultFooter">
+      <p>Footer</p>
+    </footer>
 </div>
 <?php
 include("../includes/footer.php");
---
+++ standard/tabs/bizVault/side-bar.php
@@ -0,0 +1,7 @@
+<nav class="bizVaultNav">
+    <ul>
+        <li><a href="javascript:;"><i class="fas fa-bell"></i></a></li>
+        <li><a href="javascript:;">Paris</a></li>
+        <li><a href="javascript:;">Tokyo</a></li>
+    </ul>
+</nav>
