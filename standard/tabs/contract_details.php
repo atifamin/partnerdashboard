031f7226a37e216a27ffd1dd707c44397bb8bb1d
zubair042 <zubair042@github.com>
2019-08-01 18:17:00 +0500

    commit

--- standard/tabs/contract_details.php
+++ standard/tabs/contract_details.php
@@ -235,7 +235,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-8">
 							    <label for="business_structure">Business Structure:</label>
 							    <select class="form-control" name="business_structure" id="business_structure">
-							      <option value="" selected hidden>Business Structure</option>
+							      <option value="<?php echo $user_info->business_structure; ?>" selected><?php echo $user_info->business_structure; ?></option>
 							      <option value="Sole Proprietor">Sole Proprietor</option>
 							      <option value="LLC - Limited Liability Company">LLC - Limited Liability Company</option>
 							      <option value="S-Corporation">S-Corporation</option>
@@ -247,7 +247,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-4">
 							    <label for="year_established">How Old Is Your Business:</label>
 							    <select class="form-control" name="year_established" id="year_established">
-							      <option value="" selected hidden>How Old Is Your Business</option>
+							      <option value="<?php echo $user_info->year_established; ?>" selected><?php echo $user_info->year_established; ?></option>
 							      <option value="Less Than 2 Years">Less Than 2 Years</option>
 							      <option value="3-5 Years">3-5 Years</option>
 							      <option value="5-10 Years">5-10 Years</option>
@@ -258,12 +258,12 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	</div>
 						  	<div class="form-group col-md-4">
 							    <label for="state_of_incorporation">State of Incorportation:</label>
-							    <input type="text" class="form-control" name="state_of_incorporation" id="state_of_incorporation">
+							    <input type="text" class="form-control" name="state_of_incorporation" id="state_of_incorporation" value="<?php echo $user_info->state_of_incorporation; ?>">
 							    <div class="help-block with-errors"></div>
 						  	</div>
 						  	<div class="form-group col-md-8">
 							    <label for="federal_tax_id">Federal Tax ID#:</label>
-							    <input type="text" class="form-control" name="federal_tax_id" id="federal_tax_id">
+							    <input type="text" class="form-control" name="federal_tax_id" id="federal_tax_id" value="<?php echo $user_info->federal_tax_id; ?>">
 							    <div class="help-block with-errors"></div>
 						  	</div>
 						  	<div class="form-group col-md-8">
@@ -282,7 +282,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-4">
 							    <label for="no_of_employees">No. of Employees:</label>
 							    <select class="form-control" name="no_of_employees" id="no_of_employees">
-							      <option value="" selected hidden>No. of Employees</option>
+							      <option value="<?php echo $user_info->no_of_employees; ?>" selected><?php echo $user_info->no_of_employees; ?></option>
 							      <option value="1-5">1-5</option>
 							      <option value="6-10">6-10</option>
 							      <option value="11-20">11-20</option>
@@ -298,7 +298,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-6">
 							    <label for="current_year_profit">Current Year Profit (Projected):</label>
 							    <select class="form-control" name="current_year_profit" id="current_year_profit">
-							      <option value="" selected hidden>Current Year Profit</option>
+							      <option value="<?php echo $user_info->current_year_profit; ?>" selected><?php echo $user_info->current_year_profit; ?></option>
 							      <option value="I Lost Money">I Lost Money</option>
 							      <option value="I Made No Profit">I Made No Profit</option>
 							      <option value="$1K - $10K">$1K - $10K</option>
@@ -315,7 +315,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-6">
 							    <label for="last_year_profit">Last Year Profit:</label>
 							    <select class="form-control" name="last_year_profit" id="last_year_profit">
-							      <option value="" selected hidden>Last Year Profit</option>
+							      <option value="<?php echo $user_info->last_year_profit; ?>" selected><?php echo $user_info->last_year_profit; ?></option>
 							      <option value="I Lost Money">I Lost Money</option>
 							      <option value="I Made No Profit">I Made No Profit</option>
 							      <option value="$1K - $10K">$1K - $10K</option>
@@ -332,7 +332,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-6">
 							    <label for="funding_amount">Funding Amount Requesting:</label>
 							    <select class="form-control" name="funding_amount" id="funding_amount">
-							      <option value="" selected hidden>Funding Amount Requesting</option>
+							      <option value="<?php echo $user_info->funding_amount; ?>" selected><?php echo $user_info->funding_amount; ?></option>
 							      <option value="$5K - $10K">$5K - $10K</option>
 							      <option value="$11K - $50K">$11K - $50K</option>
 							      <option value="$51K - $100K">$51K - $100K</option>
@@ -344,7 +344,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-6">
 							    <label for="use_of_funds">Use of Funds:</label>
 							    <select class="form-control" name="use_of_funds" id="use_of_funds">
-							      <option value="" selected hidden>Use of Funds</option>
+							      <option value="<?php echo $user_info->use_of_funds; ?>" selected><?php echo $user_info->use_of_funds; ?></option>
 							      <option value="Working Capital">Working Capital</option>
 							      <option value="Payroll">Payroll</option>
 							      <option value="Inventory/Materials">Inventory/Materials</option>
@@ -355,7 +355,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 						  	<div class="form-group col-md-12">
 							    <label for="currently_financed">How Currently Financed:</label>
 							    <select class="form-control" name="currently_financed" id="currently_financed">
-							      <option value="" selected hidden>How Currently Financed</option>
+							      <option value="<?php echo $user_info->currently_financed; ?>" selected><?php echo $user_info->currently_financed; ?></option>
 							      <option value="Self Financed">Self Financed</option>
 							      <option value="Bank Loan/Line of Credit">Bank Loan/Line of Credit</option>
 							      <option value="Owner Equity">Owner Equity</option>
@@ -367,19 +367,19 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 							    <label for="largest_client1">Four Largest Clients:</label>
 							    <div class="row">
 							      <div class="col-md-6">
-							        <input type="text" class="form-control" name="largest_client1" id="largest_client1">
+							        <input type="text" class="form-control" name="largest_client1" id="largest_client1" value="<?php echo $user_info->largest_client1; ?>">
 							        <div class="help-block with-errors"></div>
 							      </div>
 							      <div class="col-md-6">
-							        <input type="text" class="form-control" name="largest_client2" id="largest_client2">
+							        <input type="text" class="form-control" name="largest_client2" id="largest_client2" value="<?php echo $user_info->largest_client2; ?>">
 							        <div class="help-block with-errors"></div>
 							      </div>
 							      <div class="col-md-6">
-							        <input type="text" class="form-control" name="largest_client3" id="largest_client3">
+							        <input type="text" class="form-control" name="largest_client3" id="largest_client3" value="<?php echo $user_info->largest_client3; ?>">
 							        <div class="help-block with-errors"></div>
 							      </div>
 							      <div class="col-md-6">
-							        <input type="text" class="form-control" name="largest_client4" id="largest_client4">
+							        <input type="text" class="form-control" name="largest_client4" id="largest_client4" value="<?php echo $user_info->largest_client4; ?>">
 							        <div class="help-block with-errors"></div>
 							      </div>
 							    </div>
@@ -393,7 +393,7 @@ $businesTypes = getBusiTypeByParent("SELECT * FROM business_type WHERE parent_id
 	                <div id="form-step-03" role="form" data-toggle="validator">
 	                  <div class="form-group">
 	                    <label for="terms">I agree with the T&C</label>
-	                    <input type="checkbox" id="terms" data-error="Please accept the Terms and Conditions" required>
+	                    <input type="checkbox" id="terms" data-error="Please accept the Terms and Conditions" <?php if ($user_info->is_past_due_debts == 1) { echo "checked";} ?> required>
 	                    <div class="help-block with-errors"></div>
 	                  </div>
 	                </div>
