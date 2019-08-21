256a20a9f08626ac3cd375a1cf70d4495d5c38f1
zubair042 <zubair042@github.com>
2019-08-21 18:01:23 +0500

    commit

--- standard/tabs/apply_loan_form/step2.php
+++ standard/tabs/apply_loan_form/step2.php
@@ -9,7 +9,7 @@
   <div class="form-group col-md-8">
     <label for="business_structure">Business Structure:</label>
     <!-- <input type="text" class="form-control" name="company_type" id="company_type" > -->
-    <select class="form-control" name="business_structure" id="business_structure" >
+    <select class="form-control" name="business_structure" id="business_structure" required>
       <option value="<?php echo $userDetail->business_structure; ?>" selected hidden><?php echo $userDetail->business_structure; ?></option>
       <option value="Sole Proprietor">Sole Proprietor</option>
       <option value="LLC - Limited Liability Company">LLC - Limited Liability Company</option>
@@ -21,7 +21,7 @@
   </div>
   <div class="form-group col-md-4">
     <label for="year_established">How Old Is Your Business:</label>
-    <select class="form-control" name="year_established" id="year_established" >
+    <select class="form-control" name="year_established" id="year_established" required>
       <option value="<?php echo $userDetail->year_established; ?>" selected hidden><?php echo $userDetail->year_established; ?></option>
       <option value="Less Than 2 Years">Less Than 2 Years</option>
       <option value="3-5 Years">3-5 Years</option>
@@ -33,12 +33,12 @@
   </div>
   <div class="form-group col-md-4">
     <label for="state_of_incorporation">State of Incorportation:</label>
-    <input type="text" class="form-control" name="state_of_incorporation" id="state_of_incorporation" value="<?php echo $userDetail->state_of_incorporation; ?>" >
+    <input type="text" class="form-control" name="state_of_incorporation" id="state_of_incorporation" value="<?php echo $userDetail->state_of_incorporation; ?>" required>
     <div class="help-block with-errors"></div>
   </div>
   <div class="form-group col-md-8">
     <label for="federal_tax_id">Federal Tax ID#:</label>
-    <input type="text" class="form-control" name="federal_tax_id" id="federal_tax_id" value="<?php echo $userDetail->federal_tax_id; ?>" >
+    <input type="text" class="form-control" name="federal_tax_id" id="federal_tax_id" value="<?php echo $userDetail->federal_tax_id; ?>" required>
     <div class="help-block with-errors"></div>
   </div>
   <div class="form-group col-md-8">
@@ -58,7 +58,7 @@
   <div class="form-group col-md-4">
     <label for="no_of_employees">No. of Employees:</label>
     <!-- <input type="text" class="form-control" name="no_of_employees" id="no_of_employees" > -->
-    <select class="form-control" name="no_of_employees" id="no_of_employees" >
+    <select class="form-control" name="no_of_employees" id="no_of_employees" required>
       <option value="<?php echo $userDetail->no_of_employees; ?>" selected hidden><?php echo $userDetail->no_of_employees; ?></option>
       <option value="1-5">1-5</option>
       <option value="6-10">6-10</option>
@@ -74,7 +74,7 @@
   </div>
   <div class="form-group col-md-6">
     <label for="current_year_profit">Current Year Profit (Projected):</label>
-    <select class="form-control" name="current_year_profit" id="current_year_profit" >
+    <select class="form-control" name="current_year_profit" id="current_year_profit" required>
       <option value="<?php echo $userDetail->current_year_profit; ?>" selected hidden><?php echo $userDetail->current_year_profit; ?></option>
       <option value="I Lost Money">I Lost Money</option>
       <option value="I Made No Profit">I Made No Profit</option>
@@ -91,7 +91,7 @@
   </div>
   <div class="form-group col-md-6">
     <label for="last_year_profit">Last Year Profit:</label>
-    <select class="form-control" name="last_year_profit" id="last_year_profit" >
+    <select class="form-control" name="last_year_profit" id="last_year_profit" required>
       <option value="<?php echo $userDetail->last_year_profit; ?>" selected hidden><?php echo $userDetail->last_year_profit; ?></option>
       <option value="I Lost Money">I Lost Money</option>
       <option value="I Made No Profit">I Made No Profit</option>
@@ -109,7 +109,7 @@
   <div class="form-group col-md-6">
     <label for="funding_amount">Funding Amount Requesting:</label>
     <!-- <input type="text" class="form-control" name="funding_amount" id="funding_amount" > -->
-    <select class="form-control" name="funding_amount" id="funding_amount" >
+    <select class="form-control" name="funding_amount" id="funding_amount" required>
       <option value="<?php echo $userDetail->funding_amount; ?>" selected hidden><?php echo $userDetail->funding_amount; ?></option>
       <option value="$5K - $10K">$5K - $10K</option>
       <option value="$11K - $50K">$11K - $50K</option>
@@ -122,7 +122,7 @@
   <div class="form-group col-md-6">
     <label for="use_of_funds">Use of Funds:</label>
     <!-- <input type="text" class="form-control" name="use_of_funds" id="use_of_funds" > -->
-    <select class="form-control" name="use_of_funds" id="use_of_funds" >
+    <select class="form-control" name="use_of_funds" id="use_of_funds" required>
       <option value="<?php echo $userDetail->use_of_funds; ?>" selected hidden><?php echo $userDetail->use_of_funds; ?></option>
       <option value="Working Capital">Working Capital</option>
       <option value="Payroll">Payroll</option>
@@ -134,7 +134,7 @@
   <div class="form-group col-md-12">
     <label for="currently_financed">How Currently Financed:</label>
     <!-- <input type="text" class="form-control" name="currently_financed" id="currently_financed" > -->
-    <select class="form-control" name="currently_financed" id="currently_financed" >
+    <select class="form-control" name="currently_financed" id="currently_financed" required>
       <option value="<?php echo $userDetail->currently_financed; ?>" selected hidden><?php echo $userDetail->currently_financed; ?></option>
       <option value="Self Financed">Self Financed</option>
       <option value="Bank Loan/Line of Credit">Bank Loan/Line of Credit</option>
@@ -147,19 +147,19 @@
     <label for="largest_client1">Four Largest Clients:</label>
     <div class="row">
       <div class="col-md-6">
-        <input type="text" class="form-control" name="largest_client1" id="largest_client1"  value="<?php echo $userDetail->largest_client1; ?>">
+        <input type="text" class="form-control" name="largest_client1" id="largest_client1"  value="<?php echo $userDetail->largest_client1; ?>" required>
         <div class="help-block with-errors"></div>
       </div>
       <div class="col-md-6">
-        <input type="text" class="form-control" name="largest_client2" id="largest_client2"  value="<?php echo $userDetail->largest_client2; ?>">
+        <input type="text" class="form-control" name="largest_client2" id="largest_client2"  value="<?php echo $userDetail->largest_client2; ?>" required>
         <div class="help-block with-errors"></div>
       </div>
       <div class="col-md-6">
-        <input type="text" class="form-control" name="largest_client3" id="largest_client3"  value="<?php echo $userDetail->largest_client3; ?>">
+        <input type="text" class="form-control" name="largest_client3" id="largest_client3"  value="<?php echo $userDetail->largest_client3; ?>" required>
         <div class="help-block with-errors"></div>
       </div>
       <div class="col-md-6">
-        <input type="text" class="form-control" name="largest_client4" id="largest_client4" value="<?php echo $userDetail->largest_client4; ?>" >
+        <input type="text" class="form-control" name="largest_client4" id="largest_client4" value="<?php echo $userDetail->largest_client4; ?>" required>
         <div class="help-block with-errors"></div>
       </div>
     </div>
