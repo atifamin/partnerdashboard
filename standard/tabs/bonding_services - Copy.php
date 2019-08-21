8b9fed12774ea45104130a401ea039fea94ee85f
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-08-21 16:01:21 +0500

    commit

---
+++ standard/tabs/bonding_services - Copy.php
@@ -0,0 +1,90 @@
+<div class="row">
+  <div class="col-lg-8 col-xs-12">
+    <div class="box"> 
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover modal-financing-table contract_details_table" id="">
+
+        </table>
+      </div>
+      <!-- /.box-body --> 
+    </div>
+    <!-- /.box --> 
+    
+  </div>
+  <div class="col-lg-4 col-xs-12" align="center">
+    <div class="div-cont-amount"> <span class="container">$757,000</span> </div>
+
+  </div>
+</div>
+<div class="row cus-border mt-10">
+  <div class="col-md-4 text-center">
+    <div class="mt-30"><span class="font-30 color-1">SURETY BONDING</span> </div>
+  <a href="javascript:;" onclick="showofferboxpopup(2,'Basic Partner')" class="btn mb-10 mt-10" style="background-color: #642524;"><span class="text-white">REQUEST BONDING</span></a>
+    <div class="vl-bonding-services"></div>
+  </div>
+  <div class="col-md-5 text-center" style="padding: 1px;">
+    <div class="color-2">
+      <strong class="font-25">SURETY BONDING SERVICES</strong><br>
+      <strong style="font-style: italic;">
+        Offering a wide range of services including: <br>
+        Big Bonds <br>
+        Performance Bonds <br>
+        Payement Bonds <br>
+        Maintenance Bonds
+      </strong>
+    </div><br>
+    <div class="vl-bonding-services"></div>
+  </div>
+  <div class="col-md-3 text-center">
+    <div class="mt-10 mb-10">
+      <button type="button" class="btn custom-btn-1 mt-10"><span class="text-white font-25">CLICK HERE<br>TO GET<br>STARTED</span></button>
+    </div>
+  </div>
+</div>
+<div class="row mt-10 cus-border">
+  <div class="col-md-4 text-center mt-10 mb-10">
+    <div class="col-md-12 mb-10 text-white">
+      <div class="bg-1"><span class="font-20">YOUR bizVAULT Status</span></div>
+    </div>
+    <div class="col-md-4"><img src="../assets/img/a13.png" width="80"></div>
+    <div class="col-md-8">
+      <div class="text-white bg-2 mt-10"><span>Your bizVAULT Contains All <br>
+        Needed Documents and <br>
+        Files</span></div>
+    </div>
+    <div class="vl-1"></div>
+  </div>
+  <div class="col-md-5 text-center">
+    <div class="mt-20"> 
+      <strong class="font-13 color-3" style="color: #283644;">SURETY SERVICES PROVIDED BY NETWORKS<br>OF SURETY BROKERS AND AGENTS<br></strong>
+      <strong class="font-17" style="color: #bd6e56;">SURETY BONDING FOR CALIFORNIA<br>CERTIFIED BUSINESS</strong>
+    </div>
+    <div class="vl"></div>
+  </div>
+  <div class="col-md-3 text-center">
+    <button type="button" class="btn btn-block btn-lg bg-button mt-10" onclick="open_video_modal_bonding_services()">
+      <span class="text-white font-13">California Certified Business FastFund</span>
+      <br>
+      <i class="fab fa-youtube mt-5 font-48"></i>
+      <br>
+      <span class="text-white font-13">Contact and Invoice Financing</span>
+    </button>
+  </div>
+</div>
+
+<div class="modal fade text-center" id="modal-video-bonding-services">
+  <video class="mt-10" width="80%" controls="">
+          <source src="<?php echo base_url.$VIDEO_D['video_url_path']; ?>" type="video/mp4">
+          Your browser does not support HTML5 video. 
+    </video>
+  <!-- /.modal-dialog -->
+</div>
+
+<script type="text/javascript">
+
+  function open_video_modal_bonding_services() {
+    $('#modal-video-bonding-services').modal('show');
+    $('#modal-default').scrollTop(0);
+  }
+
+</script>
