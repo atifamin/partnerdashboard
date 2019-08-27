5706a8e58086321aaa2fd79eb2a3c7396f688b86
usamaajaved <46658189+usamaajaved@users.noreply.github.com>
2019-08-27 15:24:48 +0500

    commit

--- standard/tabs/financial_services.php
+++ standard/tabs/financial_services.php
@@ -1,16 +1,14 @@
 <?php
+
   $_FILE_NAME = basename($_SERVER['REQUEST_URI'], '?'.$_SERVER['QUERY_STRING']); 
-?>
-<?php //echo "<pre>";print_r($_SESSION); ?>
-<?php 
+
   if($_FILE_NAME == "contract_details.php"){ 
     $FirmID = $_SESSION['dbe_firm_id'];
     $VIDEO_QUERY = "select * from funding_video";
     $VIDEO_R = mysqli_query($con_MAIN,$VIDEO_QUERY) or die(mysqli_error()); 
     $VIDEO_D =  mysqli_fetch_assoc($VIDEO_R);
   }
-?>
-<?php 
+
   if($_FILE_NAME != "contract_details.php"){
     include("../includes/header.php");
     include("../includes/top_nav.php");
@@ -24,76 +22,76 @@
   $UserCertificationID = $_SESSION['certification_id'];
   $UserID = $_SESSION['user_id'];
   if($UserOfferCodes!='' && $UserOfferScore!=0){
-  $Score = explode(',',$UserOfferCodes);
-  $OfferCodes='';
-  $Index3_offer = 1;
-  foreach($Score as $key=>$val){
-    $val = str_replace(' ','',$val);
-    if($Index3_offer>1){
-      $OfferCodes .=' OR po.offer_code LIKE "%'.$val.'%"';
-    }else{
-      $OfferCodes .=' po.offer_code LIKE "%'.$val.'%"';
-    }
-    $Index3_offer++;  
-  }   
-  $Tab3_Q1 = 'SELECT *
-  FROM partner_offer po
-  LEFT JOIN user_offer_status uos ON po.partner_offer_id = uos.partner_offer_id
-  WHERE ('.$OfferCodes.') 
-  AND po.offer_score <= '.$UserOfferScore.' 
-  AND uos.offer_status_user_id = '.$UserID.'
-  AND uos.user_minimum_score <= '.$UserOfferScore.'
-  ';    
+    $Score = explode(',',$UserOfferCodes);
+    $OfferCodes='';
+    $Index3_offer = 1;
+    foreach($Score as $key=>$val){
+      $val = str_replace(' ','',$val);
+      if($Index3_offer>1){
+        $OfferCodes .=' OR po.offer_code LIKE "%'.$val.'%"';
+      }else{
+        $OfferCodes .=' po.offer_code LIKE "%'.$val.'%"';
+      }
+      $Index3_offer++;  
+    }   
+    $Tab3_Q1 = 'SELECT *
+    FROM partner_offer po
+    LEFT JOIN user_offer_status uos ON po.partner_offer_id = uos.partner_offer_id
+    WHERE ('.$OfferCodes.') 
+    AND po.offer_score <= '.$UserOfferScore.' 
+    AND uos.offer_status_user_id = '.$UserID.'
+    AND uos.user_minimum_score <= '.$UserOfferScore.'
+    ';    
 
-  $Tab3_Q1R = mysqli_query($con_MAIN,$Tab3_Q1) or die(mysqli_error());
-  if(mysqli_num_rows($Tab3_Q1R)>0){
-    while($OfferData = mysqli_fetch_assoc($Tab3_Q1R)){
-      $PartnerOfferID = $OfferData['partner_offer_id'];
+    $Tab3_Q1R = mysqli_query($con_MAIN,$Tab3_Q1) or die(mysqli_error());
+    if(mysqli_num_rows($Tab3_Q1R)>0){
+      while($OfferData = mysqli_fetch_assoc($Tab3_Q1R)){
+        $PartnerOfferID = $OfferData['partner_offer_id'];
 
-      $InsQ = "INSERT INTO `partner_offer_views`(`partner_offer_user_id`,`partner_offer_id`) VALUES('".$UserID."','".$PartnerOfferID."')";
-      mysqli_query($con_MAIN,$InsQ) or die(mysqli_error());
+        $InsQ = "INSERT INTO `partner_offer_views`(`partner_offer_user_id`,`partner_offer_id`) VALUES('".$UserID."','".$PartnerOfferID."')";
+        mysqli_query($con_MAIN,$InsQ) or die(mysqli_error());
 
-      // Partner Data
-      $Tab3_Q4 = 'SELECT * FROM `partner` WHERE `partner_id`='.$OfferData['partner_id'].'';
-      $Tab3_Q4R = mysqli_query($con_MAIN,$Tab3_Q4) or die(mysqli_error());  
-      if(mysqli_num_rows($Tab3_Q4R)>0){
-        $PartnerName1 = mysqli_fetch_array($Tab3_Q4R); 
-        $Logo = $PartnerName1['partner_logo_url'];
-        if($Logo == ''){ $Logo = 'assets/img/companylogo.png'; }
-      } else { $Logo = 'assets/img/companylogo.png'; }  
-  
-      $PartnerName = $PartnerName1['partner_name']; 
-      //UserCompany Name
-      $Tab3_Q5 = 'SELECT * FROM `sbdvbe` WHERE `Certification ID`='.$UserCertificationID.'';
-      $Tab3_Q5R = mysqli_query($con_MAIN,$Tab3_Q5) or die(mysqli_error());
-      if(mysqli_num_rows($Tab3_Q5R)>0){
-        $UserC = mysqli_fetch_array($Tab3_Q5R);
-        $UserCompany = $UserC['Legal Business Name'];
-      } else{ $UserCompany=''; }
+        // Partner Data
+        $Tab3_Q4 = 'SELECT * FROM `partner` WHERE `partner_id`='.$OfferData['partner_id'].'';
+        $Tab3_Q4R = mysqli_query($con_MAIN,$Tab3_Q4) or die(mysqli_error());  
+        if(mysqli_num_rows($Tab3_Q4R)>0){
+          $PartnerName1 = mysqli_fetch_array($Tab3_Q4R); 
+          $Logo = $PartnerName1['partner_logo_url'];
+          if($Logo == ''){ $Logo = 'assets/img/companylogo.png'; }
+        } else { $Logo = 'assets/img/companylogo.png'; }  
+    
+        $PartnerName = $PartnerName1['partner_name']; 
+        //UserCompany Name
+        $Tab3_Q5 = 'SELECT * FROM `sbdvbe` WHERE `Certification ID`='.$UserCertificationID.'';
+        $Tab3_Q5R = mysqli_query($con_MAIN,$Tab3_Q5) or die(mysqli_error());
+        if(mysqli_num_rows($Tab3_Q5R)>0){
+          $UserC = mysqli_fetch_array($Tab3_Q5R);
+          $UserCompany = $UserC['Legal Business Name'];
+        } else{ $UserCompany=''; }
 
-      // For View Log
-      $S1 = 'SELECT `finserv_id` FROM `finserv` WHERE `partner_id`='.$OfferData['partner_id'].' AND `offer_id`='.$PartnerOfferID.''; 
-      $S1R = mysqli_query($con_MAIN,$S1) or die(mysqli_error());
+        // For View Log
+        $S1 = 'SELECT `finserv_id` FROM `finserv` WHERE `partner_id`='.$OfferData['partner_id'].' AND `offer_id`='.$PartnerOfferID.''; 
+        $S1R = mysqli_query($con_MAIN,$S1) or die(mysqli_error());
 
-      $Tab2_Q5 = 'SELECT `cfm_id`,`content_code` FROM `cfm` WHERE `type`="Best Practices" LIMIT 1';
-      $Tab2_Q5R = mysqli_query($con_MAIN,$Tab2_Q5) or die(mysqli_error());
-      $Tab2_Index5 = 1;
-      $Tab2_Q5D = mysqli_fetch_assoc($Tab2_Q5R);
+        $Tab2_Q5 = 'SELECT `cfm_id`,`content_code` FROM `cfm` WHERE `type`="Best Practices" LIMIT 1';
+        $Tab2_Q5R = mysqli_query($con_MAIN,$Tab2_Q5) or die(mysqli_error());
+        $Tab2_Index5 = 1;
+        $Tab2_Q5D = mysqli_fetch_assoc($Tab2_Q5R);
 
-      if(mysqli_num_rows($S1R)>0){
-        $FinSer = mysqli_fetch_assoc($S1R);
-        $FinServID = $FinSer['finserv_id'];
-        $CFMid   = $Tab2_Q5D['cfm_id'];
-        $UserID    = $_SESSION['user_id'];
-        $Insert = "INSERT INTO `finserv_views`(`finserv_user_id`,`finserv_id`,`finserv_view_date`) VALUES ('".$UserID."','".$FinServID."','".date('Y-m-d H:i:s')."')";
-        $Tab2_Q5R = mysqli_query($con_MAIN,$Insert) or die(mysqli_error());
-      }   
+        if(mysqli_num_rows($S1R)>0){
+          $FinSer = mysqli_fetch_assoc($S1R);
+          $FinServID = $FinSer['finserv_id'];
+          $CFMid   = $Tab2_Q5D['cfm_id'];
+          $UserID    = $_SESSION['user_id'];
+          $Insert = "INSERT INTO `finserv_views`(`finserv_user_id`,`finserv_id`,`finserv_view_date`) VALUES ('".$UserID."','".$FinServID."','".date('Y-m-d H:i:s')."')";
+          $Tab2_Q5R = mysqli_query($con_MAIN,$Insert) or die(mysqli_error());
+        }   
 
-      //$OfferID = $_POST['offerid'];
-  
-      $Query = 'SELECT * FROM `offer_box` WHERE `offer_box_id`='.$OfferData['offer_box_id'].'';
-      $QueryR = mysqli_query($con_MAIN,$Query);
-      $row = mysqli_fetch_object($QueryR);
+        //$OfferID = $_POST['offerid'];
+    
+        $Query = 'SELECT * FROM `offer_box` WHERE `offer_box_id`='.$OfferData['offer_box_id'].'';
+        $QueryR = mysqli_query($con_MAIN,$Query);
+        $row = mysqli_fetch_object($QueryR);
 
 ?>
 
@@ -198,12 +196,12 @@
 </div>
 
 <?php
-    }// While close
-  }else{
-    // Find Special Case
-    include "tab3/specialcase.php"; 
-   }
-}else{ echo $NoResult; }
+      }// While close
+    } else{
+        // Find Special Case
+        include "tab3/specialcase.php"; 
+    }
+  } else{ echo $NoResult; }
 ?>
 <?php 
   if($_FILE_NAME != "contract_details.php"){
@@ -212,6 +210,19 @@
 ?>
 
 <script type="text/javascript">
+  function getFinancing(id){
+    var base_url = "<?php echo base_url; ?>";
+    $.ajax({
+        type: "POST",
+        url: ""+base_url+"tabs/tab1/contract_detail.php",
+        data: {id:id},
+        success:function(data){
+
+          $('.contract_details_table').html(data);
+        }
+      });
+  }
+
   function open_video_modal() {
     $('#modal-video').modal('show');
     $('#modal-default').scrollTop(0);
